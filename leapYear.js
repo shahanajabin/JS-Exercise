c2138570d634c156010312cc3cb45427412d990d
jabin <jabin@bititude.com>
2024-09-11 05:56:03 +0530

    deleted

--- leapYear.js
+++
@@ -1,9 +0,0 @@
-function leapYear(year) {
-    if ((year % 4 == 0 && year % 100 !== 0) || (year % 400 == 0)) {
-        console.log("This Year is a leap year");
-    } else {
-        console.log("This Year is not a Leap Year");
-    }
-    return year
-} leapYear(2032)
-
