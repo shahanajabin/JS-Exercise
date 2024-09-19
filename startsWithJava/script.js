45196ae17210a5054010e0e3d77c679fe1892893
jabin <jabin@bititude.com>
2024-09-19 13:37:46 +0530

    Check whether a string starts with 'Java' and false otherwise.

---
+++ startsWithJava/script.js
@@ -0,0 +1,12 @@
+// Check whether a string starts with 'Java' and false otherwise.
+
+let string = prompt("Enter A String.");
+
+function startWithJava() {
+
+    if (string.startsWith("Java")) {
+        alert(true)
+    } else {
+        alert(false)
+    }
+} startWithJava()
