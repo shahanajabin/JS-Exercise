f3a405a56caf025bea641d5594e71f115d99fb51
jabin <jabin@bititude.com>
2024-09-20 16:29:21 +0530

    check whether three given numbers are increasing in strict mode or in soft mode.

--- numberSequenceMode/script.js
+++ numberSequenceMode/script.js
@@ -8,7 +8,7 @@ function numberSequenceMode() {
 
     if (num1 < num2 && num2 < num3) {
         alert('It is in Strict Mode.')
-    } else if (num2 >= num1 && num2 >= num3) {
+    } else if (num2 > num1 || num3 > num1) {
         alert('It is in Soft Mode.')
     }
     else {
