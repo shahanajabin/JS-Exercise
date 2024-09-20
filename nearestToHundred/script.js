43a30e9d56f044388116de7691b9483f876d75da
jabin <jabin@bititude.com>
2024-09-20 11:31:31 +0530

    Find a value which is nearest to 100 from two different given integer values.

---
+++ nearestToHundred/script.js
@@ -0,0 +1,18 @@
+// Find a value which is nearest to 100 from two different given integer values
+
+let input1 = parseInt(prompt('Enter The First Number'));
+let input2 = parseInt(prompt('Enter The Second Number'));
+
+function nearestToHundread() {
+
+    let diffInput1 = Math.abs(input1 - 100);
+    let diffInput2 = Math.abs(input2 - 100);
+    if (diffInput1 < diffInput2) {
+        alert(input1);
+    } else if (input1 - 100 == input2 - 100) {
+        alert("Both The value are same so the difference is also same.");
+    } else {
+        alert(input2);
+    }
+
+} nearestToHundread()
