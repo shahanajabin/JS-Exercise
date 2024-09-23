42a3984e8c4ac245b3e6c8eef27abdb7baebe4e7
jabin <jabin@bititude.com>
2024-09-23 13:31:33 +0530

    Convert the letters of a given string in alphabetical order.

---
+++ alphabetizeString/script.js
@@ -0,0 +1,17 @@
+// Convert the letters of a given string in alphabetical order. 
+
+let string = prompt('Enter a String to Sort');
+
+function alphabetizeString() {
+
+    if (string !== null) {
+        let newString = string.split('').sort().join('');
+        alert(newString);
+    } else {
+        alert('Please, Enter a String.')
+    }
+
+
+
+} alphabetizeString()
+
