870a98f8967aaf107390d5610594486c8614b763
jabin <jabin@bititude.com>
2024-09-23 16:32:07 +0530

    Count the number of vowels in a given string.

--- vowelCount/script.js
+++ vowelCount/script.js
@@ -1,13 +1,13 @@
 // Count the number of vowels in a given string. 
 
-let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+let vowels = ['a', 'e', 'i', 'o', 'u']
 function vowelCount(string) {
     let result = 0;
     for (let i = 0; i < string.length; i++) {
-        if (vowels.includes(string[i])) {
+        if (vowels.includes(string[i].toLowerCase())) {
             result++;
         }
     }
     console.log(result);
 } vowelCount('hello world')
-vowelCount('Antidisestablishmentarianism')
+vowelCount('antidisestablishmentarianism')
