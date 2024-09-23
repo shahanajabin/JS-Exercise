ae95932aead69f73086cf164dffbca2365f76c0b
jabin <jabin@bititude.com>
2024-09-23 10:41:00 +0530

    initial commit

---
+++ replaceWithNextChar/script.js
@@ -0,0 +1,13 @@
+// Replace every character in a given string with the character following it in the alphabet.
+
+// let string = prompt("Enter a String!");
+
+function capitalizeWords(string) {
+
+    let newString = '';
+    for (let i = 0; i < string.length; i++) {
+        console.log(string[i]);
+
+    }
+
+} capitalizeWords('hey')
