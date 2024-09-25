65afec07780aba789364b040d53a3ddcfd01777b
jabin <jabin@bititude.com>
2024-09-25 16:47:55 +0530

    Remove a character at the specified position of a given string and return the new string.

---
+++ removeChar/script.js
@@ -0,0 +1,16 @@
+// Remove a character at the specified position of a given string and return the new string.
+
+let string = prompt('Enter a String.');
+let position = prompt('Enter the Position of Character to be Removed.');
+
+function removeChar() {
+
+    let newArray = '';
+    for (let i = 0; i < string.length; i++) {
+        if (i !== position) {
+            newArray += string[i]
+        }
+    }
+    console.log(newArray);
+
+} removeChar()
