6bc050be38a605f9a9b0d4c2fcd85d3fecffab8d
jabin <jabin@bititude.com>
2024-10-04 12:20:31 +0530

    Check whether a matrix is a diagonal matrix or not.

---
+++ diagonalMatrixCheck/script.js
@@ -0,0 +1,40 @@
+//  Check whether a matrix is a diagonal matrix or not. 
+
+function diagonalMatrixCheck(array) {
+
+    let diagonal = true;
+    let squareMatrix = array.every((matrix) =>
+        array.length == matrix.length
+    );
+
+    if (squareMatrix == true) {
+        for (let i = 0; i < array.length; i++) {
+
+            let matrix = array[i];
+            for (let j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    if (matrix[j] == 0) {
+                        diagonal = false;
+                    }
+                } else {
+                    if (matrix[j] !== 0) {
+                        diagonal = false
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+    if (diagonal == true) {
+        console.log('This Matrix is a Diagonal Matrix.');
+
+    } else {
+        console.log('This Matrix is not a Diagonal Matrix.');
+
+    }
+
+}
+diagonalMatrixCheck([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
